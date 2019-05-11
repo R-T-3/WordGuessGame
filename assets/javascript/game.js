b189da255b1a40a2f32adcac39afde7753e60096
R-T_3 <rtansill7@gmail.com>
2019-05-11 14:41:54 -0700

    initial javascript word bank and randomization

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var words = [
+    "throne", "dragon", "battle", "north", "wildlings", "south", "winter", "sword", "shield", "arrow", "raven"
+];
+
+var word = words[Math.floor(Math.random()* words.length)]
