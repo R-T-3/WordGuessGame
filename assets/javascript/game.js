9c2d29dbd3397abc009e5f1b7159372a95d63f76
R-T_3 <rtansill7@gmail.com>
2019-05-11 14:50:58 -0700

    set up answer array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,12 @@ var words = [
     "throne", "dragon", "battle", "north", "wildlings", "south", "winter", "sword", "shield", "arrow", "raven"
 ];
 
-var word = words[Math.floor(Math.random()* words.length)]
+var word = words[Math.floor(Math.random()* words.length)];
+
+answerArray = []
+
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+
+var remaingLetters = word.length;
