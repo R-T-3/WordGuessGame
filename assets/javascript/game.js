6f0fb233feee995aad807c79cc306eeb670cfad9
R-T_3 <rtansill7@gmail.com>
2019-05-11 19:14:07 -0700

    saved progress but code still not complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,41 @@
-var words = [
-    "throne", "dragon", "battle", "north", "wildlings", "south", "winter", "sword", "shield", "arrow", "raven"
-];
+document.getElementById("started").addEventListener("keypress", got);
+$(document).keydown(got);
 
-var word = words[Math.floor(Math.random()* words.length)];
+function got() {
+    console.log("got is running");
 
-answerArray = []
+    var words = [
+        "throne", "dragon", "battle", "north", "wildlings", "south", "winter", "sword", "shield", "arrow", "raven"
+    ];
+    var word = words[Math.floor(Math.random()* words.length)];
+    var answerArr = [];
+    var guessedLetters = [];
 
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
+    for (var i = 0; i < word.length; i++) {
+        answerArr[i] = " _";
+        document.getElementById("word").innerHTML = answerArr.join(" ");
+    };
 
-var remaingLetters = word.length;
+    var remainingLetters = word.length;
+    var j = 0;
+    
+    while (remainingLetters > 0) {
+        if (event.key === word[j]) {
+            console.log("it worked")
+            answerArr[j] = word[j];
+            document.getElementById("word").innerHTML = answerArr.join(" ");
+            remainingLetters--;
+        }
+        else {
+            guessedLetters.push(word[j]);
+            document.getElementById("guessed").innerHTML = guessedLetters;
+        }
+        
+        j++
+    };
+};
+
+
+
+
+
